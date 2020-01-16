7014ee195381688328a345c8d0451bb765029b97
HiltonJS <hkudya@icloud.com>
2020-01-16 17:27:10 +0200

    updating our app with a shop page,collections preview component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom'; 
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage =()=>(
 
-  <div>
-    
-    <h1>HatsPage</h1>
-
-  </div>
-)
 
 
 function App() {
@@ -18,7 +12,7 @@ function App() {
     <div >
     <Switch> 
      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
